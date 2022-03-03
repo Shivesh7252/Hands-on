471065e730c79d09345c649c0d3c5a4acdb51a4a
Shivesh Bharti <Shivesh.Bharti@genpact.com>
2022-03-03 23:20:58 +0530

    REST API using Spring Boot

---
+++ REST_Using_Spring/src/main/java/com/org/rest/RestUsingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.org.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestUsingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestUsingSpringApplication.class, args);
+	}
+
+}
