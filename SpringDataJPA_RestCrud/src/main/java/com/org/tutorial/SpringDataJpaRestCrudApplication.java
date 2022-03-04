746dd441648bcdafe2be3228c4f6d0edf029c098
Shivesh Bharti <Shivesh.Bharti@genpact.com>
2022-03-04 15:59:20 +0530

    Adding Spring boot application using RestCrud Api on 4th march

---
+++ SpringDataJPA_RestCrud/src/main/java/com/org/tutorial/SpringDataJpaRestCrudApplication.java
@@ -0,0 +1,13 @@
+package com.org.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaRestCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaRestCrudApplication.class, args);
+	}
+
+}
